1b5a41fb0cb08d025997a9559b35c521ffe1153c
js <jieseob1@gmail.com>
2022-03-29 20:17:18 +0900

    최단경로 정리

--- 인프런 강의/섹션7/최대점수 구하기.py
+++ 인프런 강의/섹션7/최대점수 구하기.py
@@ -1,16 +1,10 @@
-def dfs(L, sum, time):  # level과 sum
-    if sum > m:  # sum이 m보다 커지는 경우는 제한시간 초과
-        return
-    if sum <= m:  # 제한시간 안에 풀 경우
 
 
 n, m = map(int, input().split())
-# 문제의 개수 n, 제한 시간 m
-pv = list()  # problem value
-pt = list()  # problem time
-for _ in range(n):
-    a, b = map(int, input().split())  # 문제, 푸는데 걸리는 시간
-    pv.append(a)  # pv 부분에는 문제점수가 들어간다.
-    pt.append(b)  # pt 부분에는 제한시간이 들어간다.
-res = -2147000000  # 최소 -> 답이 될 부분
-dfs(0, 0, 0)
+pv = list()
+pt = list()
+
+for _ in range(n):  # n개의 줄만큼
+    a, b = map(int, input().split())
+    pv.append(a)
+    pt.append(b)
