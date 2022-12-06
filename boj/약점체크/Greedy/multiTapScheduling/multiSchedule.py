2a8e90c1049e5f629fabf7ef5109d33335a0a9e8
jieseob1 <jieseob1@gmail.com>
2022-12-06 23:41:30 +0900

    see hack

---
+++ boj/약점체크/Greedy/multiTapScheduling/multiSchedule.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/1700
+# 사용하고 있는 전기용품의 사용순서를 알아냄
+# 플러그를 빼는 횟수의 최소화
+
+n,k = map(int,input().split()) # n: 멀티탭 구멍의 개수 전기 용품의 총 사용횟수
+appliance_name =list(map(int,input().split()))
+# 모르겠음
