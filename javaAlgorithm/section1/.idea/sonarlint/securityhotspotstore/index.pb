9c36180c50e5040d2395fdc3a75cc0eb97d2a6a8
jiseob park <jieseob1@gmail.com>
2023-07-05 19:43:39 +0900

    7/5 Sovle two Basic java Algoritm

---
+++ javaAlgorithm/section1/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+B
+string_3/Main.java,b/a/baeb1def96371724f70a70c7cfee24dc9e5f3d80
---
+++ javaAlgorithm/section1/boj/Main.java
@@ -0,0 +1,29 @@
+//package section1.boj;
+//import java.io.*;
+//import java.util.Scanner;
+//import java.util.StringTokenizer;
+//
+//
+////https://www.acmicpc.net/problem/2110
+//public class Main {
+//    public int solution(String str, char t) {
+//        int answer = 0;
+//
+//        return answer;
+//    }
+//    public static void main(String[] args) throws IOException {
+////        스태틱에서 인스턴스 메소드 호출하려면 클래스 객체 만들어줘야 함
+//        Main T = new Main();
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+//
+//        String s = bf.readLine(); //String
+//        int i = Integer.parseInt(bf.readLine()); //Int
+////        Scanner kb=new Scanner(System.in);
+////        String str =kb.next(); //문자열을 하나 읽어들인다.
+////        char c = kb.next().charAt(0); //string을 index로 접근
+//
+//        System.out.print(T.solution(str,c));
+//    }
+//}
