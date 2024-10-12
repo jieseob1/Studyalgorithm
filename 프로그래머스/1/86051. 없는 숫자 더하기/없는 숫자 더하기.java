5d8d1b745950d929eea8e5e2c775fe12bea0304d
victor park <68308294+jieseob1@users.noreply.github.com>
2024-10-12 11:13:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,16 +1,18 @@
-import java.util.*;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Solution {
+    private static List<Integer> arr = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
     public int solution(int[] numbers) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap();
-        for(int num : numbers) {
-            map.put(num,1);
-        }
-        for(int i = 0; i <= 9; i++) {
-            if(map.getOrDefault(i,0) != 1) {
-                answer += i;
+        List<Integer> newNum = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+        for(int ele : arr) {
+            if(!newNum.contains(ele)) {
+                answer += ele;
             }
         }
+        System.out.println(answer);
         return answer;
     }
 }
