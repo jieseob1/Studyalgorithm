a19819a1482a3a96dc4e5c5fa6e3886e289965b7
victor park <68308294+jieseob1@users.noreply.github.com>
2024-10-17 20:23:50 +0900

    [level 2] Title: 타겟 넘버, Time: 5.67 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,18 +2,19 @@ class Solution {
     static int cnt = 0;
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        int sum = 0;
-        dfs(numbers,0,sum, target);
-        return cnt;
+        dfs(0,numbers[0],numbers,target);
+        dfs(0,-numbers[0],numbers,target);
+        answer = cnt;
+        return answer;
     }
-    public void dfs(int[] numbers, int depth, int sum,int target) {
-        if(numbers.length == depth) {
+    private void dfs(int v, int sum,int[] numbers, int target) {
+        if (v == numbers.length-1) {
             if (sum == target) {
                 cnt++;
             }
         } else {
-            dfs(numbers, depth+1, sum + numbers[depth],target);
-            dfs(numbers, depth+1, sum - numbers[depth],target);
+            dfs(v+1, sum+numbers[v+1], numbers, target);
+            dfs(v+1, sum-numbers[v+1], numbers, target);
         }
     }
 }
