fc537d17220dd2bfca24d723ea9fcb9c1602025a
victor park <68308294+jieseob1@users.noreply.github.com>
2024-11-12 20:04:18 +0900

    [level 3] Title: 입국심사, Time: 63.37 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,22 +1,30 @@
+import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        long start = 1;
-        long end = 1000000000 * 100000L;
-        System.out.println(end);
-        while(end > start) {
-            long cnt = 0;
-            long mid = (start + end) / 2;
-            for(int time: times) {
-                cnt += mid / time;
-            }
-            if(cnt >= n) { //cnt가 더 많다는건 mid값이 많다는 뜻
-                end = mid;
-            } else {
-                start = mid + 1;
+        long left = 1;
+        long right = n * (long) Arrays.stream(times).max().getAsInt();
+        while(left < right) { // 나중에 같아 지는 거 대비하기 위해서 등호 뺌
+            long mid = (left + right) / 2;
+            // System.out.println("mid => " + mid);
+            // System.out.println("left => " + left);
+            // System.out.println("right => " + right);
+            if (count(times, mid) >= n) { //count가 더 크면 줄여야 한다.
+                right = mid;
+            }  else {
+                left = mid + 1; 
             }
         }
-        answer = start;
-        return answer;
+        //이렇게 하면 안될거 같은데, 기준을 좀 알아야 될듯
+        System.out.println("left => " + left);
+        System.out.println("right => " + right);
+        return left;
+    }
+    
+    private static long count(int[] times, long mid) {
+        long cnt = 0;
+        for(int time : times) {
+            cnt += mid / time;
+        }
+        return cnt;
     }
 }
