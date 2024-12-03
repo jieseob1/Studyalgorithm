1241b1f881186ef2f479dd3ac29a6946b0505aa0
victor park <68308294+jieseob1@users.noreply.github.com>
2024-12-03 19:38:40 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,3 +1,3 @@
 -- 코드를 입력하세요
 -- 보호 시작일 보다 입양일이 더 빠른 동물의 아이디 animal_ins => 보호 시작일 보호 시작일이 더 커야 한다.
-SELECT a.animal_id, a.name from animal_ins a join animal_outs b on a.animal_id = b.animal_id where a.datetime > b.datetime order by a.datetime;
+SELECT a.animal_id, a.name from animal_ins a inner join animal_outs b on a.animal_id = b.animal_id where a.datetime > b.datetime order by a.datetime;
